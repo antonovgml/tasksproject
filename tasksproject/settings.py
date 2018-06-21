01b371ca1ae2c5d47160e0cef26b5587700c58ad
Andrei Antonau <aantonau@gomel.iba.by>
2018-06-21 21:46:11 +0300

    Removed unnecessary app from the project
    Change-Id: Ibc7d4fb4d974b642f753289a8a9b230079271024

--- tasksproject/settings.py
+++ tasksproject/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework', # TODO
     'tasksapp',
-    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
