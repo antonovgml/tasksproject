f225f747ab895baa7250d7f6b25e0adce05e348c
Andrei Antonau <aantonau@gomel.iba.by>
2018-06-22 10:49:30 +0300

    displayed a list of tasks on index.html
    Change-Id: Iaf3a54e22c5708620b80fb4e4c958ec925a0e8ba

--- tasksapp/views.py
+++ tasksapp/views.py
@@ -4,6 +4,7 @@ from .models import Task
 
 # Create your views here.
 def index(request):
+    tasks = Task.objects.order_by('-date_created')
+    context = {'tasks': tasks}
 
-
-    return render(request, 'tasksapp/index.html')
+    return render(request, 'tasksapp/index.html', context)
