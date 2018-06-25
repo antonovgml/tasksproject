aa5c5cd3fd353aafd4a91d397aeeb916ffaa8d0c
Andrei Antonau <aantonau@gomel.iba.by>
2018-06-25 16:27:22 +0300

    implemented DELETE for entire collection of Tasks (remove all)
    Change-Id: Ia2d43ec7d6a5f170d44f72bec9a6068baf3f1a92

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import TaskSerializer
 from .models import Task
 # from braces.views import CsrfExemptMixin
@@ -17,3 +19,13 @@ class TaskViewSet(viewsets.ModelViewSet):
     def filter_queryset(self, queryset):
         queryset = super(TaskViewSet, self).filter_queryset(queryset)
         return queryset.order_by('id')
+
+
+    # DELETE entire Tasks collection
+    def delete(self, request, *args, **kwargs):
+
+        # Task.objects.all().delete() # this might be dropped
+        # so doing this via iterator
+        for x in Task.objects.all().iterator(): x.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
