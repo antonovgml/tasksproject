9ed678d4d0053ed4f41ab1d3ad5954055481fdab
Andrei Antonau <aantonau@gomel.iba.by>
2018-06-26 17:12:25 +0300

    Got rid of view in tasksapp for serving index.html
    Change-Id: If29e0d33f6eb5eb21c8b3f2435e6672e12cdccd6

--- tasksapp/views.py
+++ tasksapp/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 
-from api.models import Task
-
 # Create your views here.
-def index(request):
-    # tasks = Task.objects.order_by('-date_created')
-    # context = {'tasks': tasks}
-
-    return render(request, 'tasksapp/index.html')
